2f50f629d9dc896efc772c78090afbca845052d5
habboubuu <houssamhabboub2000@gmail.com>
2024-10-09 18:12:34 +0000

    second commit

--- src/Components/Helmet/Helmet.js
+++ src/Components/Helmet/Helmet.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Helmet = (props) => {
-    document.title = 'Renta Car Service - ' + props.title
+    document.title = 'Renta Cars Service - ' + props.title
   return (
     <div className='w100'>
       {props.children}
